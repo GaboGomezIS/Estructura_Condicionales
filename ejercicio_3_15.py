efea5e55ffde12565c22cde720f44552753dde26
GaboGomezIS <126176494+GaboGomezIS@users.noreply.github.com>
2023-05-18 09:24:03 -0500

    Create ejercicio_3_15.py

---
+++ ejercicio_3_15.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+	# Calcular la suma de los cincuenta primeros numeros enteros.
+	# version 1.0 
+	# 11/03/2023
+	# programado por: Gabriel Gomez
+	# declaracion de variables
+	a = int()
+	s = int()
+	# inicializacion de las variables 
+	s = 0
+	for a in range(1,51): #ciclo para de los 50 primeros numero entero con paso en 1
+		s = s+a #formula de suma ,suma todos los numeros naturales hasta el 50
+	# impresion del resultado
+	print("La suma de los primeros 50 numeros enteros es: ",s)
